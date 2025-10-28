06b42368120376dea8854675ad64f7e5b3b7e736
Ryan Gibson <ryan@gibs.gg>
2025-10-27 23:00:32 -0500

    add loading animation

--- app/components/LoadingSkeleton/index.tsx
+++ app/components/LoadingSkeleton/index.tsx
@@ -1,3 +1,25 @@
+import './style.css'
+
 export default function LoadingSekelton() {
-    return <div>Loading...</div>
+    const baseClasses = 'bg-loading min-h-20 block motion-safe:animate-pulse overflow-hidden object-cover relative rounded-lg w-full h-full'
+    const portrait = 'gallery-item portrait'
+    const landscape = 'gallery-item landscape'
+    return (<div className="gallery-skeleton max-w-6xl grid w-full md:w-4/5">
+        <div className={landscape}><div className={baseClasses} /></div>
+        <div className={landscape}><div className={baseClasses} /></div>
+        <div className={portrait}><div className={baseClasses} /></div>
+        <div className={portrait}><div className={baseClasses} /></div>
+        <div className={landscape}><div className={baseClasses} /></div>
+        <div className={landscape}><div className={baseClasses} /></div>
+        <div className={portrait}><div className={baseClasses} /></div>
+        <div className={portrait}><div className={baseClasses} /></div>
+        <div className={landscape}><div className={baseClasses} /></div>
+        <div className={landscape}><div className={baseClasses} /></div>
+        <div className={portrait}><div className={baseClasses} /></div>
+        <div className={portrait}><div className={baseClasses} /></div>
+        <div className={landscape}><div className={baseClasses} /></div>
+        <div className={landscape}><div className={baseClasses} /></div>
+        <div className={portrait}><div className={baseClasses} /></div>
+        <div className={portrait}><div className={baseClasses} /></div>
+    </div>)
 }
